481959f7441426e5acc728fe1fd8f5fe1e930a8f
kbbushman <k.bushman@yahoo.com>
2021-05-21 23:59:43 -0700

    Adds get credentials

--- src/Authorization/UserCredentialsDBAccess.ts
+++ src/Authorization/UserCredentialsDBAccess.ts
@@ -27,6 +27,21 @@ export class UserCredentialsDBAccess {
     username: string,
     password: string
   ): Promise<UserCredentials | undefined> {
-    return undefined;
+    return new Promise((resolve, reject) => {
+      this.nedb.find(
+        { username, password },
+        (err: Error, docs: UserCredentials[]) => {
+          if (err) {
+            reject(err);
+          } else {
+            if (docs.length === 0) {
+              resolve(undefined);
+            } else {
+              resolve(docs[0]);
+            }
+          }
+        }
+      );
+    });
   }
 }
