a64671c92c57a5a548e159a08507baab28f21b0a
kbbushman <k.bushman@yahoo.com>
2021-05-23 16:42:09 -0700

    Updates generateToken method with updated session tokena and db query

--- src/Authorization/Authorizer.ts
+++ src/Authorization/Authorizer.ts
@@ -1,9 +1,12 @@
+import { SessionTokenDBAccess } from './SessionTokenDBAccess';
 import { UserCredentialsDBAccess } from './UserCredentialsDBAccess';
 import { Account, SessionToken, TokenGenerator } from '../Server/Model';
 
 export class Authorizer implements TokenGenerator {
   private userCredDBAccess: UserCredentialsDBAccess =
     new UserCredentialsDBAccess();
+  private sessionTokenDBAccess: SessionTokenDBAccess =
+    new SessionTokenDBAccess();
 
   async generateToken(account: Account): Promise<SessionToken | undefined> {
     const foundAccount = await this.userCredDBAccess.getUserCredentials(
@@ -12,9 +15,27 @@ export class Authorizer implements TokenGenerator {
     );
 
     if (foundAccount) {
-      return { tokenId: 'sometokenid' };
+      const token: SessionToken = {
+        accessRights: foundAccount.accessRights,
+        expirationTime: this.generateExpirationTime(),
+        username: foundAccount.username,
+        valid: true,
+        tokenId: this.generateRandomTokenId(),
+      };
+
+      await this.sessionTokenDBAccess.storeSessionToken(token);
+
+      return token;
     } else {
       return undefined;
     }
+  }
+
+  private generateExpirationTime(): Date {
+    return new Date(Date.now() + 60 * 60 * 1000);
+  }
+
+  private generateRandomTokenId(): string {
+    return Math.random().toString(36).slice(2);
   }
 }
