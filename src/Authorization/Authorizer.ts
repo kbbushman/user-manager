663d4fa3dd024c9eff5bc05c3b08a8dbd20a411a
kbbushman <k.bushman@yahoo.com>
2021-05-22 23:40:46 -0700

    Adds db and query for user in generateToken

--- src/Authorization/Authorizer.ts
+++ src/Authorization/Authorizer.ts
@@ -1,11 +1,18 @@
+import { UserCredentialsDBAccess } from './UserCredentialsDBAccess';
 import { Account, SessionToken, TokenGenerator } from '../Server/Model';
 
 export class Authorizer implements TokenGenerator {
+  private userCredDBAccess: UserCredentialsDBAccess =
+    new UserCredentialsDBAccess();
+
   async generateToken(account: Account): Promise<SessionToken | undefined> {
-    if (account.username === 'test@test.com' && account.password === '1234') {
-      return {
-        tokenId: 'sometokenid',
-      };
+    const foundAccount = await this.userCredDBAccess.getUserCredentials(
+      account.username,
+      account.password
+    );
+
+    if (foundAccount) {
+      return { tokenId: 'sometokenid' };
     } else {
       return undefined;
     }
