3e373025c43fc81dea3081f548501f6e5ef9ffcf
kbbushman <k.bushman@yahoo.com>
2021-05-24 15:05:25 -0700

    Adds http codes and methods enums

--- src/Shared/Model.ts
+++ src/Shared/Model.ts
@@ -10,3 +10,16 @@ export enum AccessRight {
 export interface UserCredentials extends Account {
   accessRights: AccessRight[];
 }
+
+export enum HTTP_CODES {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+}
+
+export enum METHODS {
+  GET = 'GET',
+  POST = 'POST',
+  DELETE = 'DELETE',
+}
