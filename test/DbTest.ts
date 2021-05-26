309f6722ef4b275d6457dd4606b4538681894bd8
kbbushman <k.bushman@yahoo.com>
2021-05-25 23:56:32 -0700

    Adds user db test

--- test/DbTest.ts
+++ test/DbTest.ts
@@ -1,11 +1,21 @@
+import { UserDBAccess } from './../src/User/UsersDBAccess';
 import { UserCredentialsDBAccess } from './../src/Authorization/UserCredentialsDBAccess';
 
 class DbTest {
   public dbAccess: UserCredentialsDBAccess = new UserCredentialsDBAccess();
+  public userDbAccess: UserDBAccess = new UserDBAccess();
 }
 
-new DbTest().dbAccess.putUserCredentials({
-  username: 'user1',
-  password: '1234',
-  accessRights: [1, 2, 3],
+// new DbTest().dbAccess.putUserCredentials({
+//   username: 'user1',
+//   password: '1234',
+//   accessRights: [1, 2, 3],
+// });
+
+new DbTest().userDbAccess.addUser({
+  age: 22,
+  email: 'jdoe@gmail.com',
+  id: '2804yshjd',
+  name: 'John Doe',
+  workingPosition: 3,
 });
