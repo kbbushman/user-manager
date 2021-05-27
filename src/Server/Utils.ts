5e2fc7d56924260d206af4ac727828955a18716c
kbbushman <k.bushman@yahoo.com>
2021-05-26 18:37:14 -0700

    Refactors baseUrl to private method, adds getUrlParameters method

--- src/Server/Utils.ts
+++ src/Server/Utils.ts
@@ -1,14 +1,25 @@
-import { URL } from 'url';
+import { URL, URLSearchParams } from 'url';
 import { IncomingMessage } from 'http';
 
 export class Utils {
+  private static getBaseUrl(req: IncomingMessage): string {
+    return `http://${req.headers.host}/`;
+  }
+
   public static getUrlBasePath(req: IncomingMessage): string {
     if (req && req.url) {
-      const baseUrl = `http://${req.headers.host}/`;
-      const parsedUrl = new URL(req.url, baseUrl);
+      const parsedUrl = new URL(req.url, this.getBaseUrl(req));
       return parsedUrl.pathname.split('/')[1];
     }
 
     return '';
+  }
+
+  public static getUrlParameters(req: IncomingMessage): URLSearchParams | undefined {
+    if (req && req.url) {
+      return new URL(req.url, this.getBaseUrl(req)).searchParams;
+    } else {
+      return undefined;
+    }
   }
 }
