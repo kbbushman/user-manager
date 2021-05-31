6bd01f371ec1b2e2d6e6905e8892fd4293df4bc2
kbbushman <k.bushman@yahoo.com>
2021-05-30 23:13:50 -0700

    Adds respondJsonObject protected method

--- src/Server/BaseRequestHandler.ts
+++ src/Server/BaseRequestHandler.ts
@@ -5,19 +5,21 @@ export abstract class BaseRequestHandler {
   protected req: IncomingMessage;
   protected res: ServerResponse;
 
-  public constructor(
-    req: IncomingMessage,
-    res: ServerResponse,
-  ) {
+  public constructor(req: IncomingMessage, res: ServerResponse) {
     this.req = req;
     this.res = res;
   }
 
-  abstract handleRequest(): Promise<void>
-   
+  abstract handleRequest(): Promise<void>;
+
   protected handleNotFound() {
     this.res.statusCode = HTTP_CODES.NOT_FOUND;
     this.res.write('Not Found');
+  }
+
+  protected respondJsonObject(code: HTTP_CODES, object: any) {
+    this.res.writeHead(code, { 'Content-Type': 'application/json' });
+    this.res.write(JSON.stringify(object));
   }
 
   protected async getRequestBody(): Promise<any> {
