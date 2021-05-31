b525f55b345904d5d75a3e8acd5abf761ea2e8a1
kbbushman <k.bushman@yahoo.com>
2021-05-31 13:04:01 -0700

    Adds public getToken method

--- src/Authorization/SessionTokenDBAccess.ts
+++ src/Authorization/SessionTokenDBAccess.ts
@@ -20,4 +20,20 @@ export class SessionTokenDBAccess {
       });
     });
   }
+
+  public async getToken(tokenId: string): Promise<SessionToken | undefined> {
+    return new Promise((resolve, reject) => {
+      this.nedb.find({ tokenId }, (err: Error, docs: any) => {
+        if (err) {
+          reject(err);
+        } else {
+          if (docs.length === 0) {
+            resolve(undefined);
+          } else {
+            resolve(docs[0]);
+          }
+        }
+      });
+    });
+  }
 }
