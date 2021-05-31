cebc629b9490c630adf8b0e668236bab4fa9c6d6
kbbushman <k.bushman@yahoo.com>
2021-05-31 12:58:12 -0700

    Adds token validation interfaces

--- src/Server/Model.ts
+++ src/Server/Model.ts
@@ -17,6 +17,21 @@ export interface SessionToken {
   accessRights: AccessRight[];
 }
 
+export enum TokenState {
+  VALID,
+  INVALID,
+  EXPIRED,
+}
+
+export interface TokenRights {
+  accessRights: AccessRight[];
+  state: TokenState;
+}
+
 export interface TokenGenerator {
   generateToken(account: Account): Promise<SessionToken | undefined>;
+}
+
+export interface TokenValidator {
+  validateToken(tokenId: string): Promise<TokenRights>;
 }
