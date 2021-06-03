4a48816466b3ccdf89a3dfbf8f7864b17870945a
kbbushman <k.bushman@yahoo.com>
2021-06-02 23:54:12 -0700

    Adds respondText protected method

--- src/Server/BaseRequestHandler.ts
+++ src/Server/BaseRequestHandler.ts
@@ -32,6 +32,11 @@ export abstract class BaseRequestHandler {
     this.res.write(message);
   }
 
+  protected respondText(httpCode: HTTP_CODES, message: string) {
+    this.res.statusCode = httpCode;
+    this.res.write(message);
+  }
+
   protected async getRequestBody(): Promise<any> {
     return new Promise((resolve, reject) => {
       let body = '';
