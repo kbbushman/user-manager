c9a5fd4b151b8d839482704aadbba2b5a9b399d3
kbbushman <k.bushman@yahoo.com>
2021-06-07 20:52:07 -0700

    Adds getUsersByName public method

--- src/User/UsersDBAccess.ts
+++ src/User/UsersDBAccess.ts
@@ -40,6 +40,19 @@ export class UserDBAccess {
     });
   }
 
+  public async getUsersByName(name: string): Promise<User[]> {
+    const regEx = new RegExp(name);
+    return new Promise((resolve, reject) => {
+      this.nedb.find({ name: regEx }, (err: Error, results: any[]) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  }
+
   private generateUserId() {
     return Math.random().toString(36).slice(2);
   }
