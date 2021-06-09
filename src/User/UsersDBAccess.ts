adceccd42ce1ea9d8f3ea8a0b7c45bcfc5e7e7cd
kbbushman <k.bushman@yahoo.com>
2021-06-08 23:59:29 -0700

    Adds deleteUserFromDb

--- src/User/UsersDBAccess.ts
+++ src/User/UsersDBAccess.ts
@@ -40,6 +40,25 @@ export class UserDBAccess {
     });
   }
 
+  private async deleteUserFromDb(userId: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.nedb.remove(
+        { id: userId },
+        (err: Error | null, numRemoved: number) => {
+          if (err) {
+            reject(err);
+          } else {
+            if (numRemoved === 0) {
+              resolve(false);
+            } else {
+              resolve(true);
+            }
+          }
+        }
+      );
+    });
+  }
+
   public async getUsersByName(name: string): Promise<User[]> {
     const regEx = new RegExp(name);
     return new Promise((resolve, reject) => {
